c854a08855d5d2255fde122af8a27b0c846dd194
kirill-stupakov <kirill.stupakov.0@gmail.com>
2021-10-01 15:13:27 +0300

    Fix: delete unused functions

--- praciceWorkOne/h1.cpp
+++ praciceWorkOne/h1.cpp
@@ -2,32 +2,6 @@
 
 using namespace std;
 
-unsigned long long quadraticTime(int n, int a, int b, int c) {
-  unsigned long long ans = 0;
-
-  for (int sumA = 2 * a; sumA <= n; sumA += 2) {
-    for (int sumB = b; sumB <= n; sumB += 2) {
-      if (c <= n - sumA - sumB) {
-        ans++;
-      }
-    }
-  }
-
-  return ans;
-}
-
-// Simplify the inner loop
-unsigned long long linearTime(int n, int a, int b, int c) {
-  unsigned long long ans = 0;
-
-  for (int sumA = 2 * a; sumA <= n - b - c; sumA += 2) {
-    ans += (n - sumA - b - c) / 2 + 1;
-  }
-
-  return ans;
-}
-
-// Simplify one more time
 unsigned long long constantTime(int n, int a, int b, int c) {
   if (n < 2*a + b + c) {
     return 0;
